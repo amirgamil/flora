0d6d17790753ead63e40f599e6718a579f83b194
amirgamil <amir.amgad01@gmail.com>
2021-08-22 01:37:55 +0400

    add rest of source files for consolidation

---
+++ map/Pluto.tsx
@@ -0,0 +1,670 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="Pluto" tilewidth="16" tileheight="16" tilecount="176" columns="22">
+ <editorsettings>
+  <export target="tileset.json" format="json"/>
+ </editorsettings>
+ <image source="../static/img/rpg.png" width="352" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Pig"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Pig"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Pig"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Pig"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Pig"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Pig"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Pig"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Pig"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Pig"/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Pig"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Pig"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Pig"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="68">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Pig"/>
+  </properties>
+ </tile>
+ <tile id="69">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Pig"/>
+  </properties>
+ </tile>
+ <tile id="70">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Pig"/>
+  </properties>
+ </tile>
+ <tile id="71">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+   <property name="metadata" value="Pig"/>
+  </properties>
+ </tile>
+ <tile id="72">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="73">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="74">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="75">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="76">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="77">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="78">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="79">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="86">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="91">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="92">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="93">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="94">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="95">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="96">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="97">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="102">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="103">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="104">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="105">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="106">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="107">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="112">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="113">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="114">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="115">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="116">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="117">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="118">
+  <properties>
+   <property name="identifier" value="Tree"/>
+  </properties>
+ </tile>
+ <tile id="119">
+  <properties>
+   <property name="identifier" value="Tree"/>
+  </properties>
+ </tile>
+ <tile id="120">
+  <properties>
+   <property name="identifier" value="Tree"/>
+  </properties>
+ </tile>
+ <tile id="121">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="122">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="123">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="124">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="125">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="134">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="135">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="136">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="137">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="138">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="139">
+  <properties>
+   <property name="identifier" value="Stop_Item"/>
+  </properties>
+ </tile>
+ <tile id="140">
+  <properties>
+   <property name="identifier" value="Tree"/>
+  </properties>
+ </tile>
+ <tile id="141">
+  <properties>
+   <property name="identifier" value="Tree"/>
+  </properties>
+ </tile>
+ <tile id="142">
+  <properties>
+   <property name="identifier" value="Tree"/>
+  </properties>
+ </tile>
+ <tile id="143">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="144">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="145">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="146">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="147">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="165">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="166">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="167">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="168">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+ <tile id="169">
+  <properties>
+   <property name="identifier" value="Background_Item"/>
+  </properties>
+ </tile>
+</tileset>
